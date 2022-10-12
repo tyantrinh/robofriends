82916f45f1cd544b94c984abe3ae26ec2653b8f5
Tyan Trinh <tyantrinh@Tyans-MacBook-Pro.local>
2022-10-11 20:03:12 -0500

    Updates

---
+++ precache-manifest.4f1d181c9a29167dab165bfde00d9cce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf36abc627fc8d2b442d",
+    "url": "/robofriends/static/css/main.17bf8269.chunk.css"
+  },
+  {
+    "revision": "bf36abc627fc8d2b442d",
+    "url": "/robofriends/static/js/main.bf36abc6.chunk.js"
+  },
+  {
+    "revision": "2e74c1474d03617ddb8f",
+    "url": "/robofriends/static/css/1.d65c8c59.chunk.css"
+  },
+  {
+    "revision": "2e74c1474d03617ddb8f",
+    "url": "/robofriends/static/js/1.2e74c147.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "30a29020d98dac9cbf61358f78cdc1e3",
+    "url": "/robofriends/index.html"
+  }
+];
